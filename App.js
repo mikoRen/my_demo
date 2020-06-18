7360978ecb583a77a5ff97d740939a059d194079
udi <udi@udideMac-mini.local>
2020-06-18 16:50:01 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+    return(
+      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+        <Text>Hello world !</Text>
+      </View>
+    )
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+ * @name: 
+ * @msg: 
+ * @param: 
+ * @return: 
+ */ 
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import HomePage from './HomePage';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => HomePage);
